46cc0cc3283db5e22fe7a8e705de4423c673ebe5
gomgomigom <nodam89@naver.com>
2021-09-08 10:09:04 +0900

    Add json.js

---
+++ javascript/json/json.js
@@ -0,0 +1,52 @@
+// JSON
+// JavaScript Object Notation
+
+// 1. Object to JSON
+// stringify(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(["apple", "banana"]);
+console.log(json);
+
+const rabbit = {
+  name: "tori",
+  color: "white",
+  size: null,
+  birthDate: new Date(),
+  jump: () => {
+    console.log(`${name} can jump!`);
+  },
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+json = JSON.stringify(rabbit, ["name", "size"]);
+console.log(json);
+
+console.log("----------------");
+
+json = JSON.stringify(rabbit, (key, value) => {
+  console.log(`key:${key}, value:${value}`);
+  return key === "name" ? "ellie" : value;
+});
+console.log(json);
+
+// 2.JSON to Object
+// parse(json)
+console.log("------------------------------------");
+json = JSON.stringify(rabbit);
+console.log(json);
+const obj1 = JSON.parse(json);
+console.log(obj1);
+const obj = JSON.parse(json, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return key === "birthDate" ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump();
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
