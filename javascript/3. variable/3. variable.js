9f8a5e1cdf750a93b1e1d30a9cf2d507b8727d4b
gomgomigom <nodam89@naver.com>
2021-09-17 13:27:06 +0900

    Rename folder

--- javascript/3. variable/3. variable.js
+++ javascript/3. variable/3. variable.js
@@ -1,15 +1,15 @@
 // 1. Use strict
 // added in ES 5
 // use this for Valina Javascript.
-"use strict";
+'use strict';
 // 2. Variable, rw(read,write)
 // let (added in ES6)
-let globalnames = "global names";
-let names = "hey";
+let globalnames = 'global names';
+let names = 'hey';
 {
-  let names = "ellie";
+  let names = 'ellie';
   console.log(names);
-  names = "hello";
+  names = 'hello';
   console.log(names);
 }
 console.log(names);
@@ -25,7 +25,7 @@ console.log(age);
 }
 console.log(age);
 
-// 3. Contant, r(read only)
+// 3. Constant, r(read only)
 // use const whenever possible.
 // only use let if variable needs to change.
 const daysInWeek = 7;
@@ -33,7 +33,7 @@ const maxNumber = 5;
 console.log(daysInWeek);
 
 // Note!
-// Immutable data types: premitive types, frozen objects (i.e. object.freeze())
+// Immutable data types: primitive types, frozen objects (i.e. object.freeze())
 // Mutable data types: all objects by default are mutable in JS
 // favor immutable data type always for a few reasons:
 //  - security
@@ -41,7 +41,7 @@ console.log(daysInWeek);
 //  - reduce human mistakes
 
 // 4. Variable types
-// primitive, single item: number, string, boolean, null, undefiedn, symbol
+// primitive, single item: number, string, boolean, null, undefined, symbol
 // object, box container
 // function, first-class function
 
@@ -49,34 +49,35 @@ const count = 17; // integer
 const size = 13.1; // decimal number
 console.log(`value: ${count}, type: ${typeof count}`);
 console.log(`value: ${size}, type: ${typeof size}`);
-let a = "안녕";
+let a = '안녕';
 let b = 4;
 console.log(`value: ${a + b}, type: ${typeof (a + b)}`);
 
 // number - speicla numeric values: infinity, -infinity, NaN
 const infinity = 1 / 0;
 const negativeInfinity = -1 / 0;
-const nAn = "not a number" / 2;
+const nAn = 'not a number' / 2;
 console.log(infinity);
 console.log(negativeInfinity);
 console.log(nAn);
 
 // bigInt (fairly new, don't use it yet)
-const bigInt =
-  134214523151563425421521521643263267325612453214637294653792856123789456128378942957162593n;
+const bigInt = 134214523151563425421521521643263267325612453214637294653792856123789456128378942957162593n;
 const number = 134214523151563425421521521643263267325612453214637294653792856123789456128378942957162593;
 
 console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
 console.log(`value: ${number}, type: ${typeof number}`);
 
 // string
-const char = "c";
-const brendan = "brendannnnn";
-const greeting = "hello" + brendan;
+const char = 'c';
+const brendan = 'brendannnnn';
+const greeting = 'hello' + brendan;
+const greeting2 = `hello ${brendan}!`;
 console.log(`value: ${greeting}, type: ${typeof greeting}`);
+console.log(`value: ${greeting2}, type: ${typeof greeting2}`);
 const helloBob = `hi ${brendan}!`; //template literals (string)
 console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
-console.log("vlaue: " + helloBob + " type:" + typeof helloBob);
+console.log('vlaue: ' + helloBob + ' type:' + typeof helloBob);
 
 // boolean
 // false: 0, null, undefined, NaN, ''
@@ -97,23 +98,23 @@ console.log(`value: ${x}, type: ${typeof x}`);
 // symbol, create unique identifiers for objects
 const symbol1 = Symbol(`id`);
 const symbol2 = Symbol(`id`);
-console.log(symbol1 === symbol2);
+console.log(symbol1 === symbol2); //false
 const gSymbol1 = Symbol.for(`id`);
 const gSymbol2 = Symbol.for(`id`);
 console.log(gSymbol1 === gSymbol2); // true
 console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
 
 // object, real-life object, data structure
-// const ellie = { name = 'ellie', age: 22 }; // 안되는데 왜 안되는지 모르겠음
-// console.log(ellie);
+const ellie = { name: 'ellie', age: 22 }; // 안되는데 왜 안되는지 모르겠음 'name =' 를 'name:'로 바꿔주니 해결
+console.log(ellie);
 
 // 5. Dynamic typing: dynamically typed language
-let text = "hello";
+let text = 'hello';
 console.log(text.charAt(0));
 console.log(`value: ${text}, type: ${typeof text}`);
 text = 1;
 console.log(`value: ${text}, type: ${typeof text}`);
-text = "7" + 5;
+text = '7' + 5;
 console.log(`value: ${text}, type: ${typeof text}`);
-text = "7" / "5";
+text = '7' / '5';
 console.log(`value: ${text}, type: ${typeof text}`);
